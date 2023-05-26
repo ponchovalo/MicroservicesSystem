ab2c8c86bb83b51fbd547e5f2ce4810c415905f9
IldefonsoValenzuela <ponchovalo@outlook.com>
2023-05-26 11:59:19 -0600

    API.SEGURIDAD

---
+++ Servicios.api.Seguridad/Core/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Servicios.api.Seguridad.Core.Entities
+{
+    public class Usuario : IdentityUser
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+
+    }
+}
---
+++ Servicios.api.Seguridad/Core/Persistance/SeguridadContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Servicios.api.Seguridad.Core.Entities;
+
+namespace Servicios.api.Seguridad.Core.Persistance
+{
+    public class SeguridadContext : IdentityDbContext<Usuario>
+    {
+        public SeguridadContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+
+    }
+}
